5072ad91fed3341318f3bb59e616a04a3b7f2ee3
redmoon <rublivsky@gmail.com>
2025-01-28 08:32:11 +0200

    fix db

--- app/database/models.py
+++ app/database/models.py
@@ -4,7 +4,10 @@ from sqlalchemy import ForeignKey, String, BigInteger, DateTime, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine, AsyncSession
 
-DATABASE_URL = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://")
+dotenv.load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL", "").replace("postgres://", "postgresql+asyncpg://")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 engine = create_async_engine(DATABASE_URL, echo=False)
 
 async_session = async_sessionmaker(engine, class_=AsyncSession)
@@ -16,7 +19,7 @@ class Base(AsyncAttrs, DeclarativeBase):
 class UsersORM(Base):
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
-    telegram_id = mapped_column(BigInteger, nullable=True)
+    telegram_id = mapped_column(BigInteger, nullable=True, unique=True)
     username: Mapped[str] = mapped_column(String(length=30), nullable=True)
     contact: Mapped[str] = mapped_column(String(length=20), nullable=True)
 
