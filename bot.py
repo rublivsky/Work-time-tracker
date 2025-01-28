5b40534060d78aec496a70db9e6c50ec9b3e83ac
redmoon <rublivsky@gmail.com>
2025-01-28 08:19:53 +0200

    some change

--- bot.py
+++ bot.py
@@ -5,11 +5,10 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 
 from app.database.models import async_main
-# from app.handlers import router
 from app.user import user_router
 
 load_dotenv()
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 async def main():
     await async_main()
